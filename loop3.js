6e1e7613beec87c3ba1a5ac21b03dace77d37ca6
Steve Hudek <sjhudek@gmail.com>
2017-10-18 09:18:14 -0600

    first commit

---
+++ loop3.js
@@ -0,0 +1,64 @@
+var students = [
+	{
+ 		name: 'Liz',
+ 		age: 25,
+ 		city: 'Boulder',
+	},
+
+	{
+ 		name: 'Meghan',
+ 		age: 27,
+ 		city: 'Denver',
+	},
+
+	{
+ 		name: 'Trent',
+ 		age: 32,
+ 		city: 'Boulder',
+	},
+
+	{
+ 		name: 'Chelsea',
+ 		age: 24,
+ 		city: 'Boulder',
+	},
+
+	{
+ 		name: 'Amir',
+ 		age: 18,
+ 		city: 'Denver',
+	}
+];
+
+/*
+// Problem 1
+for (var i = 0; i < students.length; i++) {
+	console.log(students[i].age)
+}
+*/
+
+/*
+// Problem 2
+for (var i = 0; i < students.length; i++) {
+	console.log(`${students[i].name}, ${students[i].city}`)
+}
+
+
+// Problem 3
+for (var i = 0; i < students.length; i++) {
+	console.log(`${students[i].name}, ${students[i].city}`)
+}
+
+
+// Problem 4
+for (var i = 0; i < 1; i++) {
+	console.log(`${students[0].name} is from ${students[0].city}`,`${students[2].name} is from ${students[2].city}`,`${students[3].name} is from ${students[3].city}`)
+}
+*/
+
+// Problem 5
+for (var i = 0; i < 1; i++) {
+	console.log(`${students[1].name} is older than 25`,`${students[2].name} is older than 25`)
+}
+
+
